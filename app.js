6d679be5487e9592cc1e536cdc0deff4f257b096
Abhishek Patel <98528020+Abhi8755@users.noreply.github.com>
2023-04-15 10:02:25 +0530

    navbar

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const menubar = document.getElementById('menubar')
+
+menubar.addEventListener('click',()=>{
+
+    const links = document.getElementById('links')
+    links.classList.toggle('links')
+})
